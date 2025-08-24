43872ae2275f14a2e0c43a67f09f058289f9f5d5
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-08-24 09:31:54 -0300

    Remove db replica config

--- .compose/db/00_init_db.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
-GRANT pg_read_all_data TO replicator;
-SELECT pg_create_physical_replication_slot('replication_slot');
